8aad3b36391c9f8e44fe415ced43a3939ec67224
Kharchenko Artem Vitalyevich <artkharchenko@ozon.ru>
2021-11-24 15:48:14 +0300

    Added test project and fix gitignore

---
+++ Tests/InstagramHelp.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+
+using System.Threading.Tasks;
+using InstagramApiSharp;
+using InstagramApiSharp.API;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Classes;
+using Xunit;
+
+namespace InstagramHelp.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task Test1()
+        {
+        }
+    }
+}
